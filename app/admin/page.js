f90f2929789ea9285e439f6d3dc3d151d869304d
Rohit005005 <137618172+Rohit005005@users.noreply.github.com>
2024-08-23 21:15:13 +0530

    trying 1

--- app/admin/page.js
+++ app/admin/page.js
@@ -55,72 +55,64 @@ function Admin() {
   };
 
   const onDeleteEvent = async () => {
-    const result = await db
-      .delete(events)
-      .where(
-          eq(events.id, eventList.id),
-      );
+    const result = await db.delete(events).where(eq(events.id, eventList.id));
   };
-
 
   return (
     <div>
-    <div className="text-white mx-auto w-[60%] border p-10">
-      <form onSubmit={onSubmit} className="flex flex-col gap-5">
-        <label>Event Name</label>
-        <input
-          type="text"
-          name="eventName"
-          onChange={(e) => setname(e.target.value)}
-          className="text-black"
-        />
-        <label>Event Description</label>
-        <textarea
-          name="evenDesc"
-          onChange={(e) => setdesc(e.target.value)}
-          className="text-black"
-        />
-        <input
-          type="file"
-          name="files"
-          multiple
-          onChange={(e) => {
-            const selectedFiles = Array.from(e.target.files);
-            console.log("Selected Files:", selectedFiles); // Debugging log
-            setFiles(selectedFiles);
-          }}
-        />
-        <button
-          type="submit"
-          className="text-white bg-secondary p-5"
-        >
-          Upload
-        </button>
-      </form>
+      <div className="text-white mx-auto w-[60%] border p-10">
+        <form onSubmit={onSubmit} className="flex flex-col gap-5">
+          <label>Event Name</label>
+          <input
+            type="text"
+            name="eventName"
+            onChange={(e) => setname(e.target.value)}
+            className="text-black"
+          />
+          <label>Event Description</label>
+          <textarea
+            name="evenDesc"
+            onChange={(e) => setdesc(e.target.value)}
+            className="text-black"
+          />
+          <input
+            type="file"
+            name="files"
+            multiple
+            onChange={(e) => {
+              const selectedFiles = Array.from(e.target.files);
+              console.log("Selected Files:", selectedFiles); // Debugging log
+              setFiles(selectedFiles);
+            }}
+          />
+          <button type="submit" className="text-white bg-secondary p-5">
+            Upload
+          </button>
+        </form>
       </div>
 
       <div className="mt-20 text-white flex flex-col gap-10">
-      {eventList?.map((item, index) => (
-        <div key={index} className="border flex justify-between p-10">
-          <div>
-          <p>
-            {item.eventName}
-          </p>
-            {item.images.map((image, i) => (
-              <div key={i}>
-                {image}
-              </div>
-            ))}
-          <p>
-            {item.eventDesc}
-          </p>
+        {eventList?.map((item, index) => (
+          <div key={index} className="border flex justify-between p-10">
+            <div>
+              <p>{item.eventName}</p>
+              {item.images.map((image, i) => (
+                <div key={i}>{image}</div>
+              ))}
+              <p>{item.eventDesc}</p>
+            </div>
+            <div>
+              <p
+                onClick={async () => {
+                  await onDeleteEvent();
+                }}
+              >
+                Delete
+              </p>
+            </div>
           </div>
-          <div>
-            <p onClick={async ()=>{await onDeleteEvent();}}>Delete</p>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   );
 }
