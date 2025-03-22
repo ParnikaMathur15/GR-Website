ab210d6433b1568a04e61f4eed17a35676b96181
ParnikaMathur15 <parnika155@gmail.com>
2025-03-22 10:38:49 +0530

    Minor Changes

--- app/page.js
+++ app/page.js
@@ -2,8 +2,7 @@ import About from "./_components/About";
 import { Carousel } from "./_components/Carousel";
 import Contact from "./_components/Contact";
 import Faq from "./_components/Faq";
-import Footer from "./_components/Footer";
-import Hero3 from "./_components/Hero3";
+// import Footer from "./_components/Footer";
 import Herofinal from "./_components/Herofinal";
 import Timeliner from "./_components/Timeliner";
 
--- configs/schema.js
+++
@@ -1,8 +0,0 @@
-const { pgTable, serial, text, json } = require("drizzle-orm/pg-core");
-
-export const events = pgTable("events", {
-  id: serial("id").primaryKey(),
-  eventName: text("Event Name").notNull(),
-  eventDesc: text("Event Desc").notNull(),
-  images: json('Images'),
-});
