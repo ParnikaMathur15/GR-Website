3795153a24fb8e1c87e439e6486e5047f0f52048
Tushar <info19tushar@gmail.com>
2025-03-29 11:12:23 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "#00F2FF",
+          DEFAULT: "#84FFF3",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
